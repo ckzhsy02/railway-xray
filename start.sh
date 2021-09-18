7080bb3b48ab499e49fa1ccffcee47cb6f640dd7
xrayrailway666 <90960021+xrayrailway666@users.noreply.github.com>
2021-09-18 09:20:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # configs
 AUUID=541558a0-a793-4b82-8987-fbb0f06e6001
-CADDYIndexPage=https:https://github.com/wulabing/3DCEList/archive/master.zip
+CADDYIndexPage=https://github.com/PavelDoGreat/WebGL-Fluid-Simulation/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
